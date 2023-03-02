a7055c2226758eb29ebc2b143df2af4e54db59f2
Olli Brummer <olli.brummer@knowit.fi>
2023-03-02 14:03:29 +0200

    results

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,37 +1,82 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat a-seq))
+
+(concat-elements [])            ;=> ()
+(concat-elements [[1 2]])       ;=> (1 2)
+(concat-elements [[1 2] [3 4]]) ;=> (1 2 3 4)
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+   (reduce #(str %1 " " %2) a-seq)))
+
+
+(str-cat ["I" "am" "Legend"])  ;=> "I am Legend"
+(str-cat ["I" "am" "back"])    ;=> "I am back"
+(str-cat ["more" " " "space"]) ;=> "more   space"
+(str-cat [])                   ;=>
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (rest (reduce #(conj %1 x %2) [] a-seq)))
+
+(my-interpose 0 [1 2 3])               ;=> (1 0 2 0 3)
+(my-interpose "," ["I" "me" "myself"]) ;=> ("I" "," "me" "," "myself")
+(my-interpose :a [1])                  ;=> (1)
+(my-interpose :a [])                   ;=> ()
 
 (defn my-count [a-seq]
-  :-)
+  (reduce (fn [nume _] (inc nume)) 0 a-seq))
+
+(my-count [])      ;=> 0
+(my-count [1 2 3]) ;=> 3
+(my-count [1])     ;=> 1
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce conj '() a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  [(reduce min a-seq)(reduce max a-seq)])
+
+(min-max-element [2 7 3 15 4]) ;=> [2 15]
+(min-max-element [1 2 3 4])    ;=> [1 4]
+(min-max-element [1])          ;=> [1 1]
 
 (defn insert [sorted-seq n]
-  [:-])
+  (loop [acc [n] s sorted-seq]
+    (cond
+      (empty? s) acc
+      :else (let [new-acc (if (< n (first s))
+                            (conj acc (first s))
+                            (conj (vec (drop-last acc)) (first s) n))]
+              (recur new-acc (rest s))))))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce insert [] a-seq))
+
+(defn toggle [a-set elem]
+  (if (contains? a-set elem)
+    (disj a-set elem)
+    (conj a-set elem)))
 
 (defn parity [a-seq]
-  [:-])
+  (reduce toggle #{} a-seq))
 
-(defn minus [x]
-  :-)
+(defn minus
+  ([x] (* -1 x))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(minus 2)   ;=> -2
+(minus 4 3) ;=> 1
+
+(defn count-params
+  ([& more]                   ; more than two parameters
+    (count more)))
+
+(count-params)            ;=> 0
+(count-params :a)         ;=> 1
+(count-params :a 1 :b :c) ;=> 4
 
 (defn my-* [x]
   :-)
